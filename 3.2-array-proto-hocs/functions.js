72b0ff2323901142ef8949d7935743d589e2cedd
Evgeniy Zuev <evgn09@yandex.ru>
2020-10-29 00:06:28 +0300

    Сделано дз

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,7 @@
+console.clear()
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+let getNames = () => weapons.map(weapon => weapon.name);
+let getCountReliableWeapons = (dur) => weapons.filter(weapon => weapon.durability > dur).length;
+let hasReliableWeapons = (rel) => weapons.some(weapon => weapon.initDurability > rel);
+let getReliableWeaponsNames = (rel) => weapons.filter(weapon => weapon.initDurability > rel).map(weapon => weapon.name);
+let getTotalDamage = () => weapons.reduce((totalDmg, weapon) => totalDmg + weapon.attack , 0);
