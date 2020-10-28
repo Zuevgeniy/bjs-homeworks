b55660f5ef028d7c4e74cab96438299ffcdb9e2e
Evgeniy Zuev <evgn09@yandex.ru>
2020-10-28 20:17:52 +0300

    изменено

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,23 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function() {
+   let string = this.toLowerCase().replace(/[^а-я]/g,"");
+    const len = Math.floor(string.length / 2);
+ for (let i = 0; i < len; i++)
+   if (string[i] !== string[string.length - i - 1])
+     return false;
+ return true ;
+}
+
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+   let average = 0;
+   for( i = 0; i < marks.length; i++) {
+       average += marks[i];
+   }
+   let roundAverage =  Math.round(average / marks.length);
+   return roundAverage
 }
 
 function checkBirthday(birthday) {
