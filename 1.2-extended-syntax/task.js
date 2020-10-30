2577f589aa05b2f1a97382ba01f93d49663f0511
Evgeniy Zuev <evgn09@yandex.ru>
2020-10-30 13:32:05 +0300

    сделано

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,42 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+
+    let discr = b**2 - 4 * a * c;
+    let x = [];
+
+    if (discr > 0) {
+    	let x1 = (-b + discr**0.5)/(2 * a);
+    	let x2 = (-b - discr**0.5)/(2 * a);
+
+    	x.push(x1, x2);
+    }
+
+    else if (discr == 0) {
+    	x.push(-b / (2 * a));
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+    let len = marks.length;
+
+    if (len > 0) {
+
+    	if (len > 5) {
+
+    		marks.splice(len - 5);
+    		len = 5;
+    	}
+
+    	let total = 0;
+    	for (let mark of marks) {
+    		total = total + mark;
+    	}
+
+    	return total/len;
+    }
+
+	else return 0;
 }
