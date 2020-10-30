ae3d7620b86e3be86b5166351fb8b6365d76747d
Evgeniy Zuev <evgn09@yandex.ru>
2020-10-30 13:42:46 +0300

    Домашняя работа сделана

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    percent = parseInt(percent,10);
+    contribution = parseInt(contribution,10);
+    amount = parseInt(amount,10);
+
+    if (isNaN(percent)) {
+    	return `Параметр ПРОЦЕНТ содержит неправильное значение - \"${window.percent.value}\"`;
+    }
+
+    else if (isNaN(contribution)) {
+  		return `Параметр ПЕРВОНАЧАЛЬНЫЙ ВЗНОС содержит неправильное значение - \"${window.contribution.value}\"`;
+    }
+
+    else if (isNaN(amount)) {
+  		return `Параметр СУММА КРЕДИТА содержит неправильное значение - \"${window.amount.value}\"`;
+    }
+
+    const now = new Date();
+    let S = amount - contribution;
+    let P = percent / 12 / 100;
+    let n = (date.getFullYear() - now.getFullYear()) * 12 + (date.getMonth() - now.getMonth());
+    let sum = (S * (P + P/(((1+P) ** n) - 1))) * n;
+    return Number(sum.toFixed(2));
 }
 
+
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+	if (typeof name !== "string" || name.length < 1) name = 'Аноним';
+	return `Привет, мир! Меня зовут ${name}`;
+
 }
